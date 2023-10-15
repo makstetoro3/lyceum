a85b5fcbbaebdc1d4a08e519df7a42c4f21fa9ae
Maksim <playmakcim228@gmail.com>
2023-10-16 01:31:17 +0500

    2 lesson

--- lyceum/urls.py
+++ lyceum/urls.py
@@ -16,20 +16,20 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 import about.urls
 import catalog.urls
 import homepage.urls
 
 urlpatterns = [
-    path('', include(homepage.urls)),
-    path('catalog/', include(catalog.urls)),
-    path('about/', include(about.urls)),
+    path("", include(homepage.urls)),
+    path("catalog/", include(catalog.urls)),
+    path("about/", include(about.urls)),
     path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
